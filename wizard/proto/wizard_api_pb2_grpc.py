7b12a614f3c0b273c4562b74a5900500f41604ae
dmjang <dogzz9445@gmail.com>
2022-02-21 10:05:48 +0900

    edit grpc

--- wizard/proto/wizard_api_pb2_grpc.py
+++ wizard/proto/wizard_api_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-import wizard_api_pb2 as wizard__api__pb2
+from . import wizard_api_pb2 as wizard__api__pb2
 
 
 class WizardServiceStub(object):
